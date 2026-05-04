8a54a83e7b39d06dde9d8ebb1d70547cebbdc8f6
acanwirayuda <aacanwirayuda@gmail.com>
2026-05-04 12:08:36 +0800

    koneksi database

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 echo "
 <a href='materi1.php'>Materi 1: if dan else</a><br><br>
 <a href='materi2.php'>Materi 2: looping</a><br><br>
-<a href='materi3.php'>Materi 3: </a><br><br>
+<a href='materi3.php'>Materi 3: function</a><br><br>
 <a href='materi4.php'>Materi 4: function</a><br><br>
+<a href='materi5.php'>Materi 5: acan</a><br><br>
 ";
 ?>
