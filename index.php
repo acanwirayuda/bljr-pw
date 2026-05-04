d28c963b9f808b39e12a21f2de78ed0df069eca5
acanwirayuda <aacanwirayuda@gmail.com>
2026-05-04 12:11:08 +0800

    update

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ echo "
 <a href='materi2.php'>Materi 2: looping</a><br><br>
 <a href='materi3.php'>Materi 3: function</a><br><br>
 <a href='materi4.php'>Materi 4: function</a><br><br>
-<a href='materi5.php'>Materi 5: acan</a><br><br>
+<a href='materi5.php'>Materi 5: koneksi database</a><br><br>
 ";
 ?>
